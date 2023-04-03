f7c662e6a10ac67eaf5bfe036a5eef1f358ba9b2
tsy <tsy@mail.com>
2023-04-03 14:01:08 +0300

    2.5: Forgot password

--- src/components/Authentication/ForgotPassword/ForgotPassword.jsx
+++ src/components/Authentication/ForgotPassword/ForgotPassword.jsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import './ForgotPassword.css';
-import imgX from '../../assets/icons/x1.svg';
+import imgX from '../../../assets/icons/x1.svg';
 const ForgotPassword = () => {
   const navigate = useNavigate();
   const {
@@ -16,28 +16,28 @@ const ForgotPassword = () => {
     navigate('/reset-password/:resetToke');
   };
   return (
-    <div className='login__wrap'>
-      <div className='login__container'>
-        <Link to={'..'} className='login__close'>
-          <img src={imgX} alt='close button' className='login__close-icon' />
+    <div className='forgot__wrap'>
+      <div className='forgot__container'>
+        <Link to={'..'} className='forgot__close'>
+          <img src={imgX} alt='close button' className='forgot__close-icon' />
         </Link>
-        <div className='login__head-wrap'>
-          <h2 className='login__title'>Forgot your password?</h2>
-          <p className='login__subtitle'>
+        <div className='forgot-wrap'>
+          <h2 className='forgot__title'>Forgot your password?</h2>
+          <p className='forgot__subtitle'>
             Don´t worry, well send you a link to get back in.
           </p>
         </div>
-        <form className='login__form form' onSubmit={handleSubmit(onSubmit)}>
+        <form className='forgot__form' onSubmit={handleSubmit(onSubmit)}>
           <input
-            className='form__input'
+            className='forgot__form__input'
             {...register('email', {
               required: 'Please enter your Password',
             })}
           />
           {errors.email && (
-            <span className='form__error'>This field is required</span>
+            <span className='forgot__form__error'>This field is required</span>
           )}
-          <button className='login__submitBtn' type='submit'>
+          <button className='forgot__login__submitBtn' type='submit'>
             Request password
           </button>
         </form>
