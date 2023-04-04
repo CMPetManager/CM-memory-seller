2c8982833d9547cda6c904fd3b2b20baf2f04103
Olena Tsaruk <tsaruksiter@gmail.com>
2023-04-04 11:14:13 +0200

    Fix: Fix completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,12 +10,15 @@ function Header() {
             <a href='/'>Catch the moment</a>
           </div>
           <div className='header__buttons'>
-            <span className='header__buttons_login btn'>
-              <Link to={'/login'}>Login</Link>
-            </span>
-            <span className='header__buttons_registration btn'>
-              <Link to={'/registration'}>Registration</Link>
-            </span>
+            <Link to={'/login'} className='header__buttons_login btn'>
+              Login
+            </Link>
+            <Link
+              to={'/registration'}
+              className='header__buttons_registration btn'
+            >
+              Registration
+            </Link>
           </div>
         </div>
       </header>
