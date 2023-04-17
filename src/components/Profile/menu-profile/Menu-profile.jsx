e4659af5a9a72908f356d7603adf2f1459a07669
DenMold <MoldovanovDV@gmail.com>
2023-04-17 16:44:59 +0300

    create HTML structure

---
+++ src/components/Profile/menu-profile/Menu-profile.jsx
@@ -0,0 +1,25 @@
+import './Menu-profile.css';
+import { Link } from 'react-router-dom';
+
+export const MenuProfile = () => {
+  return (
+    <div className='wraper'>
+      <div className='menu-profile'>
+        <div className='menu-profile__title'>
+          <h1>CATCH THE MOMENT</h1>
+        </div>
+        <div className='menu-profile__link'>
+          <Link to={'/Profile/Profile'} className='menu-profile__profile'>
+            Profile
+          </Link>
+          <Link to={'/Profile/Settings'} className='menu-profile__settings'>
+            Settings
+          </Link>
+          <Link to={'/Profile/LogOut'} className='menu-profile__log-out'>
+            Log out
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
