9d0e628879dc65eb3b5cc1e75f1cbf26338f86f2
Nataliya <78379183+pylkonata@users.noreply.github.com>
2023-04-27 00:26:58 +0300

    style: add styles

--- src/pages/Albums/Albums.jsx
+++ src/pages/Albums/Albums.jsx
@@ -1,11 +1,13 @@
 import './Albums.css';
+import plus from 'assets/icons/plus-circle.svg';
+import minus from 'assets/icons/minus-circle.svg';
 
 const Albums = () => {
   return (
     <div>
       <div className='albums__wrap'>
-        <div className='albums__back albums__back-part1'>Catch the</div>
-        <div className='albums__back albums__back-part2'>Memories</div>
+        <div className='albums__back albums__back-top'>Catch the</div>
+        <div className='albums__back albums__back-bottom'>Memories</div>
         <div className='albums__profile-wrap'>
           <h1 className='albums__logo'>Catch the moment</h1>
           <button className='albums__profile-btn'>S</button>
@@ -17,12 +19,24 @@ const Albums = () => {
           </p>
         </div>
         <div className='albums__control-panel'>
-          <button className='btn'>New album</button>
-          <div className='albums__checkbox-wrap'>
-            <label className='albums__checkbox-label'>
-              <input type='checkbox' />
-            </label>
-            <input type='search' placeholder='Search' />
+          <button className='btn albums__control-btn'>New album</button>
+          <div className='albums__control-wrap'>
+            <div className='albums__checkbox-wrap'>
+              <label className='albums__checkbox-label'>
+                <input type='checkbox' />
+              </label>
+              <img
+                src={plus}
+                alt='increase button'
+                className='albums__checkbox-plus'
+              />
+              <img
+                src={minus}
+                alt='decrease button'
+                className='albums__checkbox-minus'
+              />
+            </div>
+            <input type='search' placeholder='Search' className='form__input' />
           </div>
         </div>
         <div className='albums__container'>
