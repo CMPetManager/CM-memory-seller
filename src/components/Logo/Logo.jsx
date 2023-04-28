4c34e781e548ecd3ce5ad0dcbfac8e25718b679b
tsy <tsy@mail.com>
2023-04-28 16:37:14 +0300

    fix: bug

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,9 +2,5 @@ import React from 'react';
 import { Button } from '../Button/Button';
 import './Logo.css';
 export const Logo = ({ className }) => {
-  return (
-    <>
-      <Button titleButton='S' className={className ? className : 'logo'} />
-    </>
-  );
+  return <Button titleButton='S' className={className ? className : 'logo'} />;
 };
