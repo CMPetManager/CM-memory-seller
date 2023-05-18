db3d807691167bfd269e7f1d483b3cbdf5a0ca27
Nataliya <78379183+pylkonata@users.noreply.github.com>
2023-05-18 22:46:47 +0300

    refactor: update index.css, App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes, useLocation } from 'react-router-dom';
-import { useTransition, animated } from '@react-spring/web';
+import { AnimatePresence } from 'framer-motion';
 
 import './App.css';
 
@@ -8,41 +8,31 @@ import Login from './components/Authentication/Login/Login';
 import ForgotPassword from './components/Authentication/ForgotPassword/ForgotPassword';
 import ResetPassword from './components/Authentication/ResetPassword/ResetPassword';
 import Home from './pages/Home/Home';
-import Albums from './pages/Albums/Albums';
+import { Album } from './pages/Album/Album';
+import Profile from './components/Profile/profile/Profile';
 
 function App() {
   const location = useLocation();
-  const transitions = useTransition(location, {
-    from: {
-      opacity: 0,
-    },
-    enter: {
-      opacity: 1,
-    },
-    leave: {
-      opacity: 0,
-      transition: 'all 0.5s ease-in-out',
-    },
-  });
+
   return (
     <div className='app'>
-      {transitions((styles, item) => (
-        <animated.div style={styles}>
-          <Routes location={item}>
-            <Route path='/' element={<Home />}>
-              <Route path='registration' element={<Registration />} />
-              <Route path='login' element={<Login />} />
-              <Route path='forgot-password' element={<ForgotPassword />} />
-              <Route
-                path='reset-password/:resetToken'
-                element={<ResetPassword />}
-              />
-              <Route path='*' element={<div>Error</div>} />
-            </Route>
-            <Route path='/albums' element={<Albums />} />
-          </Routes>
-        </animated.div>
-      ))}
+      <AnimatePresence>
+        <Routes key={location.pathname} location={location}>
+          <Route path='/' element={<Home />}>
+            <Route path='registration' element={<Registration />} />
+            <Route path='login' element={<Login />} />
+            <Route path='forgot-password' element={<ForgotPassword />} />
+            <Route
+              path='reset-password/:resetToken'
+              element={<ResetPassword />}
+            />
+            <Route path='*' element={<div>Error</div>} />
+          </Route>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/album' element={<Album />} />
+          <Route path='/albums' element={<div>My Albums</div>} />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
