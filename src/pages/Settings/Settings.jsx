764d5e6f7641d0321c1f0fceab79bc05a6b73792
Nataliya <78379183+pylkonata@users.noreply.github.com>
2023-07-18 18:03:03 +0300

    feat: add html, css to settings component

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-
 import './Settings.css';
 
 import ProfilePanel from 'components/ProfilePanel/ProfilePanel';
@@ -8,6 +7,8 @@ import { Button } from 'components/Button/Button';
 
 const Settings = () => {
   const additionalClassBtn = 'btn-expand_top';
+  const emailVerification = 'Verified';
+
   return (
     <div className='settings'>
       <ProfilePanel activePage='settings' />
@@ -21,19 +22,23 @@ const Settings = () => {
         <div className='settings__body'>
           <div className='settings__item-wrap'>
             <div className='settings__subtitle-wrap'>
+              <div className='settings__subtitle-inner'>
+                <h3 className='settings__subtitle'>Email settings</h3>
+                <p className='settings__info-text'>
+                  naol.jonah@foundtoo.com ({emailVerification})
+                </p>
+              </div>
               <button
                 className={'settings__btn-expand ' + additionalClassBtn}
               ></button>
-              <h3 className='settings__subtitle'>Email settings</h3>
-              <p className='settings__user-email'>
-                naol.jonah@foundtoo.com
-                <span>(Verifed)</span>
-              </p>
             </div>
             <div className='settings__form'>
               <Input placeholder='Email address' type='text' />
               <Input placeholder='Email address repeat' type='text' />
-              <Button titleButton={'Change your email address'} />
+              <Button
+                titleButton={'Change your email address'}
+                className={'form__btn btn'}
+              />
             </div>
             <p className='settings__info-text'>
               Note: When you change your email, you go to the main page
