ddf4add4c7e6f048000494c56cfe0507759f4758
Nataliya <78379183+pylkonata@users.noreply.github.com>
2023-08-08 16:06:26 +0300

    feat: add saving tokens to localStorage after refreshing

--- src/hooks/useAxiosPrivate.js
+++ src/hooks/useAxiosPrivate.js
@@ -22,6 +22,7 @@ const useAxiosPrivate = () => {
       (response) => response,
       async (error) => {
         const prevRequest = error?.config;
+        console.log(error.response);
         if (error?.response?.status === 403 && !prevRequest?.sent) {
           prevRequest.sent = true;
           const newAccessToken = await refresh();
